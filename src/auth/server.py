7c666dbb5bbaa1d196155f85449314542525b7e2
Prasant <kprasant35@gmail.com>
2025-10-02 21:19:38 +0530

    Added server.py

--- src/auth/server.py
+++ src/auth/server.py
@@ -18,4 +18,44 @@ def login():
     if not auth or not auth.username or not auth.password:
         return 'missing credentials', 401
 
+    cur = mysql.connection.cursor()
+    res = cur.execute("SELECT * FROM user WHERE email=%s", (auth.username,))
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+        if auth.username != email or auth.password != password:
+            return 'invalid credentials', 401
+        else:
+            return createJWT(auth.username, os.environ.get('JWT_SECRET'), True)
+        else:
+            return 'invalid credentials', 401
+
+@server.route('/validate', methods=['post'])
+def validate():
+    encoded_jwt = request.headers['Authorization']
+    if not encoded_jwt:
+        return "missing credentials", 401
     
+    try:
+        decoded = jwt.decode(encoded_jwt, os.environ.get('JWT_SECRET'), algorithms=["HS256"])
+    except:
+        return "not authorized", 403
+    
+    return decoded, 200
+
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1)
+            "iat": datetime.datetime.now(tz=datetime.timezone.utc),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256"
+    )
+
+if __name__ == '__main__':
+    server.run(host='0.0.0', port=5000)
